7fbd459bc2913c9f1aff807a98b8711c03756575
hoangnh2912 <neckgamervn@gmail.com>
2023-09-02 01:32:52 +0700

    init source

---
+++ src/providers/web3.provider.ts
@@ -0,0 +1,18 @@
+import { ABI_MUSIC } from '@constants';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth/node_modules/web3-eth-contract';
+
+const web3 = new Web3(`${process.env.NETWORK_RPC}`);
+
+const newContract = (abi: any, address: string): Contract => {
+  return new web3.eth.Contract(abi, address);
+};
+
+const getBlockByNumber = async (blockNumber: number) => {
+  return await web3.eth.getBlock(blockNumber);
+};
+
+const MusicContract = newContract(ABI_MUSIC.Music.abi, ABI_MUSIC.Music.address);
+const MarketContract = newContract(ABI_MUSIC.MusicMarket.abi, ABI_MUSIC.MusicMarket.address);
+
+export { web3, newContract, MusicContract, MarketContract, getBlockByNumber };
